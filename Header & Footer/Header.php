598ba690b8df6a92d2396bc9c2ccabf4642f985b
Wessel Harm <harmwessel@gmail.com>
2026-05-08 08:45:16 +0100

    added HEader and footer files

--- Header & Footer/Header.php
+++ Header & Footer/Header.php
@@ -9,11 +9,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pixel Playground Header</title>
     <link rel="stylesheet" href="/style/style.css">
 </head>
 <body>
     
+
+
+
+
+
 </body>
 </html>
 
