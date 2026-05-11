da2f873df0706a68ad592fd21efe21c3fde8cd46
Wessel Harm <harmwessel@gmail.com>
2026-05-11 12:41:15 +0100

    Made and sligtly animated footer
    Added footer, changed most header id to fit footer id. Used transition to alter color upon hovering.

---
+++ inc/Footer.php
@@ -0,0 +1,9 @@
+<footer id="baseFooter">
+        <nav id="homeFoot">
+            <img src="\Pixel-Playground\Stijlgids\Nodehub Textless full.png" alt="Nodehub Banner" title="Nodehub Banner">
+
+            <a href="\Pixel-Playground\index.php" class="headerNav">Home</a>
+            <a href="\Pixel-Playground\games.php" class="headerNav">Games</a>
+            <a href="" class="headerNav">Help</a>
+        </nav>
+</footer>
