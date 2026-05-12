ea2a64ed42d9d3af734ad686e03396f1f96c8efe
Bas <6059520@mborijnland.nl>
2026-05-12 13:24:27 +0200

    merge

--- inc/Footer.php
+++ inc/Footer.php
@@ -1,4 +1,3 @@
-<footer id="baseFooter">
         <nav id="homeFoot">
             <a id="footerLogo" href="index.php"><img src="\Pixel-Playground\img\NodeHub-homelogo-transparant.png" alt="Nodehub Banner" title="Nodehub Banner"></a>
 
@@ -7,4 +6,3 @@
             <a href="" class="headerNav">Help</a>
             <a href="privecy.php" class="headerNav">Privecy</a>
         </nav>
-</footer>
--- inc/Header.php
+++ inc/Header.php
@@ -4,23 +4,17 @@
 
 
 
-    <header>
 
+<nav id="homeNav">
+    <section id="logo">
+        <a href="index.php"><img src="\Pixel-Playground\img\NodeHub-homelogo-transparant.png" alt="Nodehub Banner" title="Nodehub Banner"></a>
+        <a id="logoText" href="index.php"><h1>Node<span>Hub</span></h1></a> 
+    </section>
     
 
-        <nav id="homeNav">
-            <section id="logo">
-                <a href="index.php"><img src="\Pixel-Playground\img\NodeHub-homelogo-transparant.png" alt="Nodehub Banner" title="Nodehub Banner"></a>
-                <a href="index.php"><h1>Node<span>Hub</span></h1></a> 
-            </section>
-            
+    <a href="\Pixel-Playground\index.php" class="headerNav">Home</a>
+    <a href="\Pixel-Playground\games.php" class="headerNav">Games</a>
+    <a href="" class="headerNav">Help</a>
 
-            <a href="\Pixel-Playground\index.php" class="headerNav">Home</a>
-            <a href="\Pixel-Playground\games.php" class="headerNav">Games</a>
-            <a href="" class="headerNav">Help</a>
-
-            <a id="loginButton" href="/Pixel-Playground/login.php" class="btn">Login</a>
-        </nav>
-
-        
-    </header>
+    <a id="loginButton" href="/Pixel-Playground/login.php" class="btn">Login</a>
+</nav>
