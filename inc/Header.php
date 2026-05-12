538c43e77681b9c3d5edb73b40249ab1d8eb06d2
Bas <6059520@mborijnland.nl>
2026-05-12 06:08:55 +0200

    change fotos

--- inc/Header.php
+++ inc/Header.php
@@ -9,7 +9,7 @@
     
 
         <nav id="homeNav">
-            <img src="\Pixel-Playground\Stijlgids\NodeHubBanner.png" alt="Nodehub Banner" title="Nodehub Banner">
+            <img src="\Pixel-Playground\img\NodeHubBannerTranseparantNoborder.png" alt="Nodehub Banner" title="Nodehub Banner">
 
             <a href="\Pixel-Playground\index.php" class="headerNav">Home</a>
             <a href="\Pixel-Playground\games.php" class="headerNav">Games</a>
