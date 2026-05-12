c3758c2dc3504559f27afe4c3f67c8bf4bb7ef68
Bas <6059520@mborijnland.nl>
2026-05-12 06:20:50 +0200

    logo met aparte text

--- inc/Header.php
+++ inc/Header.php
@@ -9,7 +9,11 @@
     
 
         <nav id="homeNav">
-            <img src="\Pixel-Playground\img\NodeHubBannerTranseparantNoborder.png" alt="Nodehub Banner" title="Nodehub Banner">
+            <section id="logo">
+                <img src="\Pixel-Playground\img\NodeHub-homelogo-transparant.png" alt="Nodehub Banner" title="Nodehub Banner">
+                <h1>Node<span>Hub</span></h1>
+            </section>
+            
 
             <a href="\Pixel-Playground\index.php" class="headerNav">Home</a>
             <a href="\Pixel-Playground\games.php" class="headerNav">Games</a>
