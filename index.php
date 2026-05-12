5b55d15baebbe8309e5e7410aec714c5ac858992
Wessel Harm <harmwessel@gmail.com>
2026-05-12 11:12:55 +0100

    working on highscore
    Will be changed serverly

--- index.php
+++ index.php
@@ -15,15 +15,24 @@
     
     <section id="indexSection">
         <article>
-            <h1 class="homePageTitles">Games</h1>
+            <h1 class="homePageGames">Games</h1>
                 <img src="\Pixel-Playground\images\Cookieclicker Img.jpg" class="indexImages">
                 <img src="\Pixel-Playground\images\Luigi poker.png" class="indexImages">
                 <img src="\Pixel-Playground\images\RPS img.jpg" class="indexImages">
         </article>
 
-        <article>
-            <h1 class="homePageTitles">Highscores</h1>
-        </article>
+
+    <!-- Deze hieronder werkt niet goed, maak hiervan ordered list, maar nog wel een gridpatroon.-->
+
+    <article>
+        <h1 class="homePageScores">Highscores</h1>
+            <div class="highscore-grid">
+            <p class="highscoreFrames">Overall Highscores</p>
+            <p class="highscoreFrames">Cookie Clicker Highscores</p>
+            <p class="highscoreFrames">Luigi Poker Highscores</p>
+            <p class="highscoreFrames">RPS Highscores</p>
+            </div>
+    </article>
     </section>
 
 
