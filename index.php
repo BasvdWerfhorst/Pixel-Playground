33a96cd93948fde50218a3d171ba7ab7667da508
Wessel Harm <harmwessel@gmail.com>
2026-05-13 12:55:07 +0100

    Change titles to be next to eachother

--- index.php
+++ index.php
@@ -13,41 +13,32 @@
     <main>
 
     
-    <section id="indexSection">
-        <article>
-<<<<<<< HEAD
-            <h1 class="homePageGames">Games</h1>
-=======
-            <h1 class="homePageTitles">Top Games</h1>
->>>>>>> 3b3e2a9f5f57e501fc847d0a3b0d082f8dc80e81
+ <section class="indexSection">
+    <article>
+            <h1 class="homePageGames">Top Games</h1>
                 <img src="\Pixel-Playground\images\Cookieclicker Img.jpg" class="indexImages">
                 <img src="\Pixel-Playground\images\Luigi poker.png" class="indexImages">
                 <img src="\Pixel-Playground\images\RPS img.jpg" class="indexImages">
-        </article>
-
-<<<<<<< HEAD
-
-    <!-- Deze hieronder werkt niet goed, maak hiervan ordered list, maar nog wel een gridpatroon.-->
-
-    <article>
-        <h1 class="homePageScores">Highscores</h1>
-            <div class="highscore-grid">
-            <p class="highscoreFrames">Overall Highscores</p>
-            <p class="highscoreFrames">Cookie Clicker Highscores</p>
-            <p class="highscoreFrames">Luigi Poker Highscores</p>
-            <p class="highscoreFrames">RPS Highscores</p>
-            </div>
     </article>
-=======
-        <article>
-            <h1 class="homePageTitles">Highscores</h1>
-            <section id="highscores">
-                
-            </section>
+       
+ 
+    <article>
+        <h1 class="homePageGames">hello</h1>
+        <ol>
+            Hello
+            bye
+        </ol>
+    </article>
 
-        </article>
->>>>>>> 3b3e2a9f5f57e501fc847d0a3b0d082f8dc80e81
     </section>
+
+
+
+
+
+
+
+
 
 
 
