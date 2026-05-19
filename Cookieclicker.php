059d399dbf2855091151d9b89f4d3d0d834bcde0
Wessel Harm <harmwessel@gmail.com>
2026-05-19 10:42:07 +0100

    Added previously made cookie clicker
    added style, javascript, cookie img(no bg) made it fit current style

--- Cookieclicker.php
+++ Cookieclicker.php
@@ -5,13 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="\Pixel-Playground\style\style.css">
+
+    <script src="\Pixel-Playground\Javascript\Cookieclicker.js" defer></script>
 </head>
 <body>
     <header>
         <?php require 'inc/Header.php' ?>
     </header>
-    <main>
+    <main id="cookie-main">
+        <header class="title">Cookie Clicker</header>                
+        <detail class="cookies">
+            <div id="cookie-counter">0</div>
+            cookies
+        </detail>
+        <img id="cookie" src="\Pixel-Playground\img\cookie-removebg-preview.png" alt="cookie">
 
+        <button id="autocounter">Autoclicker</button>
+        <style>
+            #autocounter {
+                margin: 10px;
+            }
+        </style>
     </main>
     <footer>
         <?php require 'inc/Footer.php' ?>
---
+++ Javascript/Cookieclicker.js
@@ -0,0 +1,80 @@
+cookie = document.querySelector("#cookie");
+counter = document.querySelector("#cookie-counter");
+count = 0;
+autoClicker = document.querySelector("#autocounter");
+
+let tapcount = 1;
+
+// cookieClick function hier:
+
+function addCounter(event) {
+    count = count + 1;
+
+    counter.textContent = count;
+}
+
+function tap(event) {
+    count = count + tapcount;
+    counter.textContent = count;
+}
+setInterval(tap, 100);
+
+function button(event) {
+    if(tapcount == 1) {
+        tapcount = 0;
+    }   else {
+            tapcount = 1;
+        }
+    }
+
+
+
+// add event listener op de cookie hier:
+
+cookie.addEventListener('click', addCounter)
+
+autoClicker.addEventListener('click', button)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onEvent(event) {
+//     event: [object MouseEvent]
+//     event.target: [object HTMLImageElement]
+//     event.target.id: cookie
+//     event.clientX (muis X): 216
+//     event.clientY (muis Y) 480
+// }
