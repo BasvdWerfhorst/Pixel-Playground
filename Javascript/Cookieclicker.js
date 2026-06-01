a5fa73eaa8967e1c3a94f0c958a59cf365daf1ff
Wessel Harm <harmwessel@gmail.com>
2026-06-01 13:26:18 +0100

    Finished oven JS And started Bakery JS

--- Javascript/Cookieclicker.js
+++ Javascript/Cookieclicker.js
@@ -1,11 +1,25 @@
 let cookie = document.querySelector("#cookie");
 let counter = document.querySelector("#cookie-counter");
 let count = 0;
-
 let autoCount = 0;
-
-let prijsOven = 10;
 let ovenCount = 0;
+let bakeryCount = 0;
+//De variabelen voor de oven amount teller
+let ovenNumberElement = document.getElementById("purchaseNumberLowOven"); 
+let ovenNumberCount = 0;
+// Variabelen oven prijs
+let prijsTotaalOven = document.getElementById("prijsOven");
+let prijsOven = 10;
+// Variabelen Bakery
+let bakery = document.getElementById("BakeryImg");
+let amountBakery = document.getElementById("purchaseNumberLowBakery");
+let costBakery = document.getElementById("prijsBakery"); 
+let prijsBakeryGetal = 50; 
+let bakeryAmount = 0;
+
+
+
+
 // Base cookie clicker
 
 function addCounter(event) {
@@ -24,7 +38,8 @@ cookie.addEventListener('click', addCounter)
 // functie maken waar de event listner naar verwijst. 
 
 let oven = document.getElementById("cookie-oven-img")
-oven.addEventListener('click', ovenKoop);
+
+oven.addEventListener('click', ovenKoop); //De event listner die luistert naar de oven
 
 setInterval(autoCounter, 1000) 
 
@@ -37,37 +52,51 @@ function autoCounter() {
 
 
 
-function ovenKoop() {
-// dan koop ik èèn oven
-
+function ovenKoop() { //hier roep ik de oven eventlistner op.
 
 if (count >= prijsOven) { 
     ovenCount += 1;
     count = count - prijsOven;
-    prijsOven = prijsOven * Math.floor(1+ (0.10*ovenCount)); // math.floor rond het getal af naar beneden.
+    prijsOven = Math.floor(prijsOven * (1 + (0.10 * ovenCount))); // math.floor rond het getal af naar beneden.
+    prijsTotaalOven.innerText = "Cost: " + prijsOven; 
     counter.textContent = count; 
     autoCount = autoCount + 1;
+    ovenNumberCount = ovenNumberCount + 1;
+    ovenNumberElement.innerText = ovenNumberCount;
+
 } else {
     alert("Niet genoeg koekjes");
 }
-
-
-
-
-
-// //   En er word bijgehouden hoeveel ovens ik heb gekocht.
-// ovens moet + 1
  }
 
 
+function buyBakery() {
+    if (count >= prijsBakeryGetal) {
+        count = count - prijsBakeryGetal;
+        bakeryAmount = bakeryAmount + 1;
+        amountBakery.innerText = bakeryAmount;
+
+        prijsBakeryGetal = Math.floor(prijsBakeryGetal * (1 + (0.80 * bakeryAmount)));
+        costBakery.innerText = "Cost: " + prijsBakeryGetal;
+    } else {
+        alert("Niet genoeg koekjes");
+    }
+}
+
+
+setInterval(bakeryTime, 1000);
 
 
 
+function bakeryTime() {
+    if (bakeryAmount > 0) {
+        count = count + (count * (bakeryAmount + 0.001) * bakeryAmount);
+        counter.textContent = Math.floor(count);
+    }
+}
 
 
-
-
-
+bakery.addEventListener('click', buyBakery);
 
 
 
