d6d06f47da2ee56c81384167fa0f4352b15109d1
Bas <6059520@mborijnland.nl>
2026-06-01 14:10:57 +0200

    wincheck

--- tic tac toe.php
+++ tic tac toe.php
@@ -13,26 +13,31 @@
     </header>
     <main id="ticTacToe-background">
     <h1 id="title-ticTacToe">tic tac toe</h1>
-    <section id="tic-tac-toe">
-        <img id="1-1" class="ticTacToe-x-o" alt="">
-        <img id="1-2" class="ticTacToe-x-o" alt="">
-        <img id="1-3" class="ticTacToe-x-o" alt="">
-        
-        <img id="2-1" class="ticTacToe-x-o" alt="">
-        <img id="2-2" class="ticTacToe-x-o" alt="">
-        <img id="2-3" class="ticTacToe-x-o" alt="">
-        
-        <img id="3-1" class="ticTacToe-x-o" alt="">
-        <img id="3-2" class="ticTacToe-x-o" alt="">
-        <img id="3-3" class="ticTacToe-x-o" alt="">
-    </section>
+    <section id="ticTacMain">
+        <section id="tic-tac-toe">
+            <img id="1-1" class="ticTacToe-x-o" alt="">
+            <img id="1-2" class="ticTacToe-x-o" alt="">
+            <img id="1-3" class="ticTacToe-x-o" alt="">
+            
+            <img id="2-1" class="ticTacToe-x-o" alt="">
+            <img id="2-2" class="ticTacToe-x-o" alt="">
+            <img id="2-3" class="ticTacToe-x-o" alt="">
+            
+            <img id="3-1" class="ticTacToe-x-o" alt="">
+            <img id="3-2" class="ticTacToe-x-o" alt="">
+            <img id="3-3" class="ticTacToe-x-o" alt="">
+        </section>
 
-    <section>
-        <h3 id="ticTacToe-turn"></h3>
+        <section id="aside">
+            <section>
+                <h3 id="ticTacToe-turn"></h3>
+            </section>
+            <section id="ticTacToe-actions">
+                <button id="nextround-ticTacToe">next/start round</button>
+            </section>
+        </section>
     </section>
-    <section id="ticTacToe-actions">
-        <button id="nextround-ticTacToe">next/start round</button>
-    </section>
+    
     <section id="start-ticTacToe-box">
         <button id="start-ticTacToe" onclick="startgame()">start</button>
     </section>
