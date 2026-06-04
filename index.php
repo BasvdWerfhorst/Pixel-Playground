8f9ffeaa9fb36e27f15f25871612019872796b2c
Bas <6059520@mborijnland.nl>
2026-06-04 17:33:47 +0200

    fix

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     
     <section id="indexSection">
         <article>
-            <h1 class="homePageGames">Games</h1>
+            <h1 class="homePageGames">Top Games</h1>
                 <img src="\Pixel-Playground\images\Cookieclicker Img.jpg" class="indexImages">
                 <img src="\Pixel-Playground\images\Luigi poker.png" class="indexImages">
                 <img src="\Pixel-Playground\images\RPS img.jpg" class="indexImages">
